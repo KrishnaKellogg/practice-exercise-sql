56d6594179d4e9daf54455d59effc7dfbb109dc8
Krishna Surakanti <krishna.surakanti@kellogg.northwestern.edu>
2023-04-14 13:07:24 +0000

    attempted 7

--- practice-7.sql
+++ practice-7.sql
@@ -17,3 +17,7 @@
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
 
+SELECT property_type, COUNT(reviews.id), date_reviewed
+FROM listings INNER JOIN reviews ON listings.id = reviews.listing_id
+GROUP BY property_type
+ORDER BY date_reviewed; 
